e006585fefd3bab5c9b8a7f45ac395439a3018f4
newEndavour <36076458+newEndeavour@users.noreply.github.com>
2019-01-07 00:54:08 +0000

    Add files via upload

--- Timer555Monostable.h
+++ Timer555Monostable.h
@@ -17,7 +17,22 @@
   Credits: Library initially inspired by/ derived from "CapacitiveSensor.h" by Paul Bagder & Paul Stoffregen. 
 	- Direct I/O through registers and bitmask (from OneWire library)
 	- Thanks.
+
   Copyright (c) 2018 Jerome Drouin  All rights reserved.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
   
 */
 
